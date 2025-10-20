d8b46d4ab6ce2895e0c6a5fd96d1d988397b23c1
*EIDG6TQk4d <todoroki.kyohei@tenpos.com>
2025-10-20 11:36:28 +0900

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// import partytown from '@astrojs/partytown';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [],
+  output: 'static',
+});
